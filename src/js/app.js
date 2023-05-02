792eaad987fd8bdb4051be8ec2b6f67d7b71222e
Tryd0g0lik <work80@mail.ru>
2023-05-02 16:29:10 +0700

    delete the method ,parsing(datas) and both methods the load(), parsing() join into the method load()

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,57 @@
-import { default as read } from "./reader";
-import { default as json } from "./parser";
+import { default as read } from "./reader.js";
+import { default as json } from "./parser.js";
 
 
 export default class GameSavingLoader {
+	// parsing(datas) {
+	// 	return new Promise((resolve, reject) => {
+	// 		const response = json(datas);
+	// 		resolve(response),
+	// 			reject(
+	// 				new Error(err.message),
+	// 			);
+	// 	});
+	// }
 	load() {
-		return Promise.resolve(read());
-	}
-
-	parsing(datas) {
 		return new Promise((resolve, reject) => {
-			const response = json(datas);
-			resolve(response),
-				reject(
-					new Error(err.message),
-				);
+			const resp = read()
+				.then(item => {
+					const respone = json(item);
+					return respone;
+				});
+			resolve(resp), reject(
+				new Error(err.message),
+			);
 		});
+
 	}
 }
 
-// const response = new GameSavingLoader();
-// response.load()
-// 	.then(
-// 		saving => response.parsing(saving),
-// 		err => {
-// 			console.log(`ERROR: ${err}`);
-// 		},
-// 	)
-// 	.then(resp => console.log(resp));
+const response = new GameSavingLoader();
+response.load()
+	.then(
+		(sav) => {
+			const answer = JSON.parse(sav);
+			return class GameSaving {
+				constructor() {
+					this.ind = answer.id;
+					this.createds = answer.created;
+					this.userInfos = {
+						id: answer.userInfo.id,
+						name: answer.userInfo.name,
+						Hitman: answer.userInfo.Hitman,
+						level: answer.userInfo.level,
+						points: answer.userInfo.points
+					}
+				}
+			};
+
+		},
+		err => {
+			console.log(`ERROR: ${err}`);
+		},
+	)
+	.then(item => {
+		const objClassGameSaving = new item();
+		console.log("========>>: ", objClassGameSaving)
+	});
